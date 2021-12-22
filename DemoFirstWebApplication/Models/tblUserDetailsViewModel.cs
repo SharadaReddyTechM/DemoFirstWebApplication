db94056ccd8dd428b8a5b88cbc75a36abd49a647
SharadaReddyTechM <33647767+SharadaReddyTechM@users.noreply.github.com>
2021-12-22 18:55:48 +0530

    Add files via upload

---
+++ DemoFirstWebApplication/Models/tblUserDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoFirstWebApplication.Models
+{
+    partial class tblUserDetail
+    {
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Please enter confirm password")]
+        [Compare("password",ErrorMessage ="Password and Confirm Password mismatches")]
+        public string confirmpassword { get; set; }
+    }
+}
